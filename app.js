a43ff7720e0a6b12014a12de74096424ac2df186
HyoL <hyolim.je9@gmail.com>
2020-03-17 08:36:47 -0700

    Add heroku settings

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express"); 
 const app = express();
 
@@ -12,8 +14,14 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
-
 const User = require("./models/User")
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 // .connect returns a promise
 // if it fails, we want to know about it: catch
